9168fba2d6a7d89ba1a49b85dd3b4891e9529fde
v-losharma <v-losharma@microsoft.com>
2023-06-03 00:49:20 +0530

    full weather check project

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests as re
 import time
 city_name=input("Please enter thr city name:-")
-URL="http://api.weatherstack.com/current?access_key={access_key}&query={city}".format(access_key="b56497aaf014461730bc0ccd4a0e463f",city=city_name)
+URL="http://api.weatherstack.com/current?access_key={access_key}&query={city}".format(access_key="put_your_api_token",city=city_name)
 #print(URL)
 current_weather=re.get(URL)
 print({city_name: "{0}C".format(current_weather.json()["current"]["temperature"])})
